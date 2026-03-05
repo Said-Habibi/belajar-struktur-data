02c3d64a487e9bcfb561f510badf2fd2aea17634
Said <elekgaming008@gmail.com>
2026-03-05 11:17:49 +0700

    Update hitungUmur method to include name parameter and enhance output

--- MethodProcedureDanFunction.java
+++ MethodProcedureDanFunction.java
@@ -1,10 +1,21 @@
 public class MethodProcedureDanFunction {
-    public void hitungUmur(int tahunLahir){
+    public void hitungUmur(int tahunLahir, String nama){
         int umur = 2026 - tahunLahir;
         System.out.println("Umur saia " + umur);
+        System.out.println("Nama saia "+nama);
     }
+    public int hitungUmurIndonesia(){
+        int umurIndonesia = 2026 - 1945;
+        return umurIndonesia;
+    }
+
     public static void main(String[] args) {
         MethodProcedureDanFunction iniObject = new MethodProcedureDanFunction();
-        iniObject.hitungUmur(2007);
+        iniObject.hitungUmur(2007, "Said");
+
+        int simpan = iniObject.hitungUmurIndonesia();
+        System.out.println(simpan);
+
+        System.out.println(iniObject.hitungUmurIndonesia());
     }
 }
